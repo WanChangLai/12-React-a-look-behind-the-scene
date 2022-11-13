f2c3dab6d2235c7fedaad1798a86535520ffae62
Wan Chang Lai <wanchang.lai@live.com>
2022-11-13 16:50:38 +0800

    Add DemoOutput

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React,{ useState } from 'react';
 import Button from './components/UI/Button';
 
 import './App.css';
+import DemoOutput from './components/UI/Demo/DemoOutput';
 
 function App() {
 
   const [showParagraph,setShowParagraph] = useState(false);
+
+  console.log("App Running")
 
   const toggleParagraphHandler = () => {
     setShowParagraph((prevShowParagraph) =>!prevShowParagraph );
@@ -14,7 +17,7 @@ function App() {
   return (
     <div className='app'>
       <h2>Hi There</h2>
-      {showParagraph && <p>This is new!!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!!</Button>
     </div>
   );
